ce4665b6acf85d8732b19b18b0e009facae12d05
Brittany <brithamilton1@gmail.com>
2017-07-27 20:23:19 -0400

    new functons in angular

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
  var app = angular.module('myModule', []);
  app.controller('myController', function($scope){
-      $scope.toDo = ['workout', 'go to work', 'read'];
+    $scope.toDo = ['workout', 'go to work', 'read'];
     $scope.addToDo = function (add) {
           $scope.toDo.push(add);
-          console.log("hey");
-          console.log($scope.toDo);
-      }
+          $scope.add='';
+        }
     });
